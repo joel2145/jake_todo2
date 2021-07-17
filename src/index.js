f7ee60c61ee283107eeb2aa040e318efa75d273c
KikuchiYuta <kikkun0513@gmail.com>
2021-07-17 22:45:41 +0900

    完了ボタンを作成

--- src/index.js
+++ src/index.js
@@ -21,7 +21,37 @@ const onClickAdd = () => {
     const doneButton = document.createElement("button");
     doneButton.innerText = "完了";
     doneButton.addEventListener("click", () => {
-        alert("完了");
+        // 押された完了ボタンのliタグを削除する
+        const Target = doneButton.closest(".todo-item")
+        document.getElementById("todo-list").removeChild(Target);
+
+        // 完了リストに追加する要素を指定
+        // liを生成
+        const li = document.createElement("li");
+        li.className = "done-item";
+        // divを生成
+        const div = document.createElement("div");
+        div.className = "list-row";
+        // pを生成
+        const p = document.createElement("p");
+        p.innerText = inputText;
+        // 戻すボタンを生成
+        const returnButton = document.createElement("button");
+        returnButton.innerText = "戻す";
+        returnButton.addEventListener("click", () => {
+            // 押された戻すボタンのliタグを削除する
+            const Target = returnButton.closest(".done-item")
+            document.getElementById("done-list").removeChild(Target);
+
+
+        });
+
+        //　親子関係を設定
+        li.appendChild(div);
+        div.appendChild(p);
+        div.appendChild(returnButton);
+        // 完了リストに追加
+        document.getElementById("done-list").appendChild(li);
     });
 
     // 削除ボタンを生成
@@ -40,7 +70,6 @@ const onClickAdd = () => {
     div.appendChild(p);
     div.appendChild(doneButton);
     div.appendChild(deleteButton);
-
 
     // 未完了リストに追加
     document.getElementById("todo-list").appendChild(li);
